31856dd674f96af9bac3bead68575d8c246075ae
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-06-13 22:23:43 -0400

    Add update
    Update RetroHealthDisplay.cs

--- RetroHealthDisplay.cs
+++ RetroHealthDisplay.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace RetroHealthDisplay
 {
@@ -16,7 +17,9 @@ namespace RetroHealthDisplay
 		public static GameObject Canvas => GameObject.Find("PlayerHUD/HelmetOnUI/UICanvas/GaugeGroup");
 		public static GameObject HealthSilhouette => Canvas.transform.Find("HealthSilhouette").gameObject;
 		public static GameObject Health => Canvas.transform.Find("Health").gameObject;
-		public static GameObject VitalsText => Canvas.transform.Find("VitalsText").gameObject;
+		public static Text HealthText => Health.GetComponentInChildren<Text>();
+		public static GameObject Vitals => Canvas.transform.Find("VitalsText").gameObject;
+		public static Text VitalsText => Vitals.GetComponent<Text>();
 
 		public void Awake()
 		{
@@ -29,7 +32,19 @@ namespace RetroHealthDisplay
 		{
 			HealthSilhouette.SetActive(false);
 			Health.SetActive(true);
-			VitalsText.SetActive(true);
+			Vitals.SetActive(true);
+		}
+
+		[HarmonyPostfix, HarmonyPatch(typeof(HUDCanvas), nameof(HUDCanvas.UpdateHealth))]
+		public static void HUDCanvas_UpdateHealth(HUDCanvas __instance)
+		{
+			float healthFraction = __instance._playerResources.GetHealthFraction();
+			float health = __instance._playerResources.GetHealth();
+			HealthText.text = Mathf.Max(0f, health).ToString("F1");
+			float t = Mathf.Clamp01((healthFraction - 0.2f) * 1.25f);
+			var color = Color.Lerp(new Color(1f, 0.2f, 0.1f, 1f), new Color(1f, 1f, 1f, 1f), t);
+			HealthText.color = color;
+			VitalsText.color = color;
 		}
 	}
 
